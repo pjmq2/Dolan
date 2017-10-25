48115838cecbb1d0be21b39216a7d49d5766c104
b50587 <julian.arguedastorres@gmail.com>
2017-10-25 10:29:28 -0600

    Generación de ID

---
+++ IngeDolan3.0/IDGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+    public class IDGenerator
+    {
+        public string IntToStringFast(int value)
+        {
+            // 32 is the worst cast buffer size for base 2 and int.MaxValue
+            char[] baseChars = new char[] { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'};
+            int i = 32;
+            char[] buffer = new char[i];
+            int targetBase = baseChars.Length;
+
+            do
+            {
+                buffer[--i] = baseChars[value % targetBase];
+                value = value / targetBase;
+            }
+            while (value > 0);
+
+            char[] result = new char[32 - i];
+            Array.Copy(buffer, i, result, 0, 32 - i);
+
+            return new string(result);
+        }
+    }
