085ee72a5743671bc34a1590a501723d41471836
jourlez <josue.rojassequeira@ucr.ac.cr>
2017-10-25 22:27:07 -0600

    branch BG e HI

---
+++ IngeDolan3.0/Controllers/BacklogController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IngeDolan3._0.Models;
+
+namespace IngeDolan3._0.Controllers
+{
+    public class BacklogController : Controller
+    {
+        private dolan2Entities db = new dolan2Entities();
+
+        /*public List<Project> GetProjects()
+        {
+            var v = (from a in db.Projects select a);
+            return v.ToList();
+        }*/
+
+        public ActionResult Index()
+        {
+            /*List<Project> data = new List<Project>();
+            data = GetProjects();
+            List<SelectListItem> item = new List<SelectListItem>();
+            foreach (var c in data)
+            {
+                item.Add(new SelectListItem
+                {
+                    Text = c.ProjectName,
+                    Value = c.ProjectName.ToString()
+                });
+            }
+
+            ViewBag.projs = item;
+            return View();*/
+            ViewBag.ProjectName = new SelectList(db.Projects, "ProjectName", "ProjectName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Project model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
