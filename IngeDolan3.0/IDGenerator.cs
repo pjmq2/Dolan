34b110556e843dc1b0467f9aaaca12c086a0d3bb
b50587 <julian.arguedastorres@gmail.com>
2017-11-01 14:48:55 -0600

    ...

--- IngeDolan3.0/IDGenerator.cs
+++ IngeDolan3.0/IDGenerator.cs
@@ -7,7 +7,7 @@ using System.Web;
     {
         public string IntToString(int value)
         {
-            // 32 is the worst cast buffer size for base 2 andint.MaxValu
+            // 32 is the worst cast buffer size for base 2 andint.MaxValue
             char[] baseChars = new char[] { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'};
             int i = 32;
             char[] buffer = new char[i];
