9e1cb42456566258cb884270fe77248a7d7f040b
Julian Arguedas <julian.arguedastorres@gmail.com>
2017-11-27 01:12:23 -0600

    Milestoned!

--- IngeDolan3.0/Models/Milestone.cs
+++ IngeDolan3.0/Models/Milestone.cs
@@ -11,13 +11,16 @@ namespace IngeDolan3._0.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Milestone
     {
         public string ProjectID { get; set; }
         public int SprintID { get; set; }
         public string StoryID { get; set; }
         public string TaskID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> Date { get; set; }
         public string MilestoneID { get; set; }
         public string Description { get; set; }
